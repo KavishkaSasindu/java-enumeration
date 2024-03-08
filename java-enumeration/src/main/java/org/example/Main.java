ad3eecdf40a3768f577b6259f7a86056f64bd184
KavishkaSasindu <sasindu0215@gmail.com>
2024-03-08 18:26:56 +0530

    updated 2 by ksdev

--- java-enumeration/src/main/java/org/example/Main.java
+++ java-enumeration/src/main/java/org/example/Main.java
@@ -1,16 +1,22 @@
 package org.example;
 
 enum Month{
-    January,February,March,April,May
+    JANUARY(1),FEBRUARY(2),MARCH(3),APRIL(4),MAY(5);
+
+    final int number;
+    Month(int number){
+        this.number = number;
+    }
 }
 
 public class Main{
     public static void main(String[] args){
 
 
-        Month month1 = Month.January;
+        Month month = Month.JANUARY;
 
-        System.out.println(month1);;
+        System.out.println(month);
+        System.out.println(month.number);
     }
 
 
